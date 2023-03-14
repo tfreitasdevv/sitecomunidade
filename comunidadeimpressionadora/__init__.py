633e2ce20451f817a9703909998c3d050c8c50d4
Thiago Fonseca de Freitas <52219586+tfreitasdevv@users.noreply.github.com>
2023-03-14 18:47:41 -0300

    Update __init__.py

--- comunidadeimpressionadora/__init__.py
+++ comunidadeimpressionadora/__init__.py
@@ -2,17 +2,17 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-# import os
-# import sqlalchemy
+import os
+import sqlalchemy
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'fe1517d3024040a3c1f648d2ff5dbb66'
 
-# if os.getenv("DATABASE_URL"):
-#     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
-# else:
-#     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+if os.getenv("DATABASE_URL"):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
@@ -21,14 +21,14 @@ login_manager.login_view = 'login'
 login_manager.login_message_category = 'alert-info'
 login_manager.login_message = 'É necessário fazer login para acessar esta página'
 
-# from comunidadeimpressionadora import models
-# engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
-# if not engine.has_table("usuario"):
-#     with app.app_context():
-#         database.drop_all()
-#         database.create_all()
-#         print("Base de dados criada")
-# else:
-#     print("Base de dados já existente")
+from comunidadeimpressionadora import models
+engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+if not engine.has_table("usuario"):
+    with app.app_context():
+        database.drop_all()
+        database.create_all()
+        print("Base de dados criada")
+else:
+    print("Base de dados já existente")
 
 from comunidadeimpressionadora import routes
